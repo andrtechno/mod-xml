ccb5a5b9d449b77ed93ad49cf728077cf66ce310
Andrew <andrew.panix@gmail.com>
2018-09-04 04:01:55 +0300

    init

--- Module.php
+++ Module.php
@@ -1,10 +1,9 @@
 <?php
 /**
  * XmlModule
- * 
- * @author CORNER CMS development team <dev@corner-cms.com>
- * @license http://corner-cms.com/license.txt CORNER CMS License
- * @link http://corner-cms.com CORNER CMS
+ *
+ * @author PIXELION CMS development team <dev@pixelion.com.ua>
+ * @link http://pixelion.com.ua PIXELION CMS
  * @package modules
  * @subpackage commerce.xml
  * @uses WebModule
